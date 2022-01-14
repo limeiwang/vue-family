d36114989b4a4fb5a410e34f88a74c5930ad0418
lmw <17803125899@163.com>
2022-01-14 17:51:35 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+    <div>{{ count }}</div>
+    <div
+        w:bg="blue-400 hover:blue-500 dark:blue-500 dark:hover:blue-600"
+        w:text="sm white"
+        w:font="mono light"
+        w:p="y-2 x-4"
+        w:border="2 rounded blue-200"
+        @click="count++"
+    >
+        dianji
+    </div>
+</template>
+
+<style>
+</style>
